241b4a8ba729d6a367f8e1971a2a883cba354286
max-pc <mosleh.hesam@yahoo.com>
2021-10-09 11:58:41 +0330

    -App config.
    (cherry picked from commit 23843c64571cc361968abfb7051898fba9da45c1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeModule } from './coffee/coffee.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeeModule,
     MongooseModule.forRoot('mongodb://localhost:27017/coffee'),
   ],

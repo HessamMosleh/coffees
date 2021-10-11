4163024ea76e6b42fea8fd83c8f66aae63a1e989
max-pc <mosleh.hesam@yahoo.com>
2021-10-11 23:42:44 +0330

    -Add Exception filters.
    (cherry picked from commit 3782d70d5639087d022bc174ae5462d8bfa8a2ae)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
